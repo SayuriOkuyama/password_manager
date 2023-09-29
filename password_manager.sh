b1584903deb56e13e2a2844725ddf84d1892c1a3
Sayuri <mosyaneko77@gmail.com>
2023-09-30 03:33:25 +0900

    GnuPGを利用しファイルを暗号化できるようにしました。

--- password_manager.sh
+++ password_manager.sh
@@ -13,35 +13,67 @@ do
 
     read -p  "次の選択肢から入力してください(Add Password/Get Password/Exit)：" menu
 
-    if [[ $menu = "Add Password" ]] ; then 
-        read -p "サービス名を入力してください：" service
-        read -p "ユーザー名を入力してください：" user
-        read -p "パスワードを入力してください：" password
+    case  "$menu"  in
+        "Add Password" )
+            read -p "サービス名を入力してください：" service
+            read -p "ユーザー名を入力してください：" user
+            read -p "パスワードを入力してください：" password
+            info="サービス名:${service} ユーザー名:${user} パスワード:${password}" 
+            
+            # ファイルを復号
+            # GPG パスフレーズを渡し、バッチ処理で database.txt.gpg を復号、database.txt に出力
+            cat passphrase.txt | gpg --passphrase-fd 0 --batch -q --output database.txt --decrypt database.txt.gpg
 
-        info="サービス名:${service} ユーザー名:${user} パスワード:${password}" 
-        # ファイルに出力
-        echo $info >> database.txt
+            # 入力された値をファイルに追記
+            echo $info >> database.txt
+            
+            # ファイルを暗号化
+            # GPG パスフレーズを渡して、バッチ処理で database.txt を暗号化した database.txt.gpg 作成
+						# database.txt.gpg がすでに存在しているというエラーを無視して上書きするために --yes を記述
+            cat passphrase.txt | gpg --passphrase-fd 0 --batch --yes -c database.txt
+            
+            # database.txt を削除
+            shred -u database.txt
 
-        echo -e "\nパスワードの追加は成功しました。"
-
-    elif [[ $menu = "Get Password" ]] ; then
-        read -p "サービス名を入力してください：" service
-        # ファイルから該当するWordが含まれる行を検索
-        info=$(fgrep "サービス名:${service}" database.txt)
-
-        # 変数が空（=該当する行を取得できなかった）の場合
-        if [[ -z $info ]] ; then
-            echo "そのサービスは登録されていません。"
+            echo -e "\nパスワードの追加は成功しました。"
+        ;;
         
-        else
-            # スペースを改行に変えて出力
-            echo -e $info"\n" | tr ' ' '\n'
-        fi
-    elif [[ $menu = "Exit" ]] ; then
-        echo "Thank you!"
-        break
+        "Get Password" )
+            read -p "サービス名を入力してください：" service
 
-    else
+            # ファイルを復号
+            # GPG パスフレーズを渡し、バッチ処理で database.txt.gpg を復号、database.txt に出力
+            cat passphrase.txt | gpg --passphrase-fd 0 --batch -q --output database.txt --decrypt database.txt.gpg
+            
+            # ファイルから該当する文字が含まれる行を検索して取得
+            info=$(fgrep "サービス名:${service}" database.txt)
+
+            # ファイルを暗号化
+            # GPG パスフレーズを渡して、バッチ処理で database.txt を暗号化した database.txt.gpg 作成
+            cat passphrase.txt | gpg --passphrase-fd 0 --batch --yes -c database.txt
+
+            # database.txt を削除
+            shred -u database.txt
+
+            # 変数が空（=該当する行を取得できなかった）の場合
+            if [[ -z $info ]] ; then
+                echo "そのサービスは登録されていません。"
+            
+            else
+                # スペースを改行に変えて出力
+                echo -e $info"\n" | tr ' ' '\n'
+            fi
+        ;;
+
+        "Exit" )
+            echo "Thank you!"
+            break
+        ;;
+
+        *)
+        # 選択がどれにも当てはまらない場合
         echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
-    fi
+        ;;
+    esac
+
 done
